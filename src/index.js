11ade47a9a4a97d18e4e17100c58375e56d2e7e1
bharathrams <bharathram@navadhiti.com>
2025-02-25 12:52:01 +0530

    added csp

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./assets/styles/index.scss";
 import store from "./store/configureStore";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { getCSP } from "./csp";
+import { getCSP } from "./csp";
 
 // // Dynamically inject CSP meta tag
-// const injectCSP = () => {
-//   const cspContent = getCSP(process.env); // Pass environment variables
-//   const metaTag = document.createElement("meta");
-//   metaTag.httpEquiv = "Content-Security-Policy";
-//   metaTag.content = cspContent.trim();
-//   document.head.appendChild(metaTag);
-// };
+const injectCSP = () => {
+  const cspContent = getCSP(process.env); // Pass environment variables
+  const metaTag = document.createElement("meta");
+  metaTag.httpEquiv = "Content-Security-Policy";
+  metaTag.content = cspContent.trim();
+  document.head.appendChild(metaTag);
+};
 
-// // Inject CSP before rendering the app
-// injectCSP();
+let interval = setInterval(() => {
+  injectCSP();
+  clearInterval(interval); // Stops the interval after the first execution
+}, 5000);
 
-render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
         <App />
       </Provider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
