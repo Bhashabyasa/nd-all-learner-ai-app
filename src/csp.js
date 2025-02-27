389bffc37ac6424dfcd9a23a5307ce2d45fb743c
Sureshkumar R <sureshkumar@navadhiti.com>
2025-02-27 11:43:00 +0530

    remove the csp for the staging branch

--- src/csp.js
+++ src/csp.js
@@ -1,10 +1,12 @@
 export const getCSP = (envVariables) => {
   const {
     REACT_APP_HOST,
-    REACT_APP_LEARNER_AI_BASE_URL,
-    REACT_APP_AWS_S3_BUCKET_CONTENT_URL,
+    REACT_APP_LEARNER_AI_ORCHESTRATION_HOST,
+    REACT_APP_CONTENT_SERVICE_APP_HOST,
+    REACT_APP_LEARNER_AI_APP_HOST,
     REACT_APP_AWS_S3_BUCKET_URL,
-    REACT_APP_CSP_APP_HOST,
+    REACT_APP_AWS_S3_BUCKET_CONTENT_URL,
+    REACT_APP_TRUSTED_ORIGIN,
   } = envVariables;
   return `
       default-src 'none';
@@ -14,7 +16,7 @@ export const getCSP = (envVariables) => {
       font-src 'self' https://fonts.gstatic.com https://fonts.cdnfonts.com;
       img-src 'self' data: https://images.squarespace-cdn.com ${REACT_APP_AWS_S3_BUCKET_CONTENT_URL} ${REACT_APP_AWS_S3_BUCKET_URL};
       media-src 'self' blob: ${REACT_APP_AWS_S3_BUCKET_URL} ${REACT_APP_AWS_S3_BUCKET_CONTENT_URL};
-      connect-src 'self' ${REACT_APP_HOST} ${REACT_APP_LEARNER_AI_BASE_URL} ${REACT_APP_CSP_APP_HOST} blob:;
+      connect-src 'self' ${REACT_APP_HOST} ${REACT_APP_LEARNER_AI_ORCHESTRATION_HOST} ${REACT_APP_CONTENT_SERVICE_APP_HOST} ${REACT_APP_LEARNER_AI_APP_HOST} ${REACT_APP_AWS_S3_BUCKET_URL} ${REACT_APP_AWS_S3_BUCKET_CONTENT_URL} blob:;
       form-action 'self';
       frame-src 'self' https://www.google.com https://www.gstatic.com;
       object-src 'none';
--- src/index.js
+++ src/index.js
@@ -9,18 +9,18 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { getCSP } from "./csp";
 
 // // Dynamically inject CSP meta tag
-const injectCSP = () => {
-  const cspContent = getCSP(process.env); // Pass environment variables
-  const metaTag = document.createElement("meta");
-  metaTag.httpEquiv = "Content-Security-Policy";
-  metaTag.content = cspContent.trim();
-  document.head.appendChild(metaTag);
-};
+// const injectCSP = () => {
+//   const cspContent = getCSP(process.env); // Pass environment variables
+//   const metaTag = document.createElement("meta");
+//   metaTag.httpEquiv = "Content-Security-Policy";
+//   metaTag.content = cspContent.trim();
+//   document.head.appendChild(metaTag);
+// };
 
-let interval = setInterval(() => {
-  injectCSP();
-  clearInterval(interval); // Stops the interval after the first execution
-}, 5000);
+// let interval = setInterval(() => {
+//   injectCSP();
+//   clearInterval(interval); // Stops the interval after the first execution
+// }, 5000);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
