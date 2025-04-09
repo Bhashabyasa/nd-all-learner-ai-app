6e94f874664a8b415ede8cfe92c78f8e266e819a
Sureshkumar R <sureshkumar@navadhiti.com>
2025-04-09 10:15:08 +0530

    new branch code with updated our backend service

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import './assets/styles/index.scss';
-import store from './store/configureStore';
-import './index.css';
-render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root'),
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./assets/styles/index.scss";
+import store from "./store/configureStore";
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
